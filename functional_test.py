faa9290188c839b91f200018dd0eeca7ab82f7d7
schildchase <schildchase@qq.com>
2024-09-25 10:35:06 +0800

    add a method to easy

--- functional_test.py
+++ functional_test.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # eva 听说有个在线办事应用
         # 他去看了这个应用的首页
@@ -24,7 +29,6 @@ class NewVisitorTest(unittest.TestCase):
 
         #应用邀请他输入一个代办事项
         input_box = self.browser.find_element(By.ID,'id_new_item')
-        #_by_tag_id('id_new_item')
         self.assertEqual(
             input_box.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -38,27 +42,25 @@ class NewVisitorTest(unittest.TestCase):
         #代办事项表格中显示了“1.Buy peacock feathers”
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_table('1:Buy peacock')
 
-        table = self.browser.find_element(By.ID,'id_list_table')
-        #_by_tag_id('id_list_table')
-        rows = table.find_elements(By.ID,'tr')
-        #_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock' for row in rows),
-            "New to-do item did not appear in table"
-        )
 
         #页面中又显示了一个文本框，可以输入其他待办事项
         #他输入了“Use peacock feathers to make a fly”(使用孔雀羽毛做一个假蝇)
         #eva 做事很有调理
-        self.fail('Finish the test!')
+        input_box = self.browser.find_element(By.ID,'id_new_item')
+        input_box.send_keys('Use peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         #页面再次更新，他的清单中显示了这两个待办事项
+        self.check_for_row_in_list_table('1:Buy peacock')
+        self.check_for_row_in_list_table('2:Use peacock feathers')
 
         #eva想知道这个网站是否会记住他的清单
         #他看到网站为他生成了一个唯一的URL
         #而且页面中有些文字解说这个功能
-
+        self.fail('Finish the test!')
         #他访问这个URL，发现他的待办事项列表还在
 
         #他很满意，去睡觉了
--- lists/tests.py
+++ lists/tests.py
@@ -5,3 +5,8 @@ class HomePageTest(TestCase):
     def test_use_home_template(self):
         responses = self.client.get("/")
         self.assertTemplateUsed(responses, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        responses = self.client.post('/', data={'item_text':'A new list item'})
+        self.assertIn('A new list item', responses.content.decode())
+        self.assertTemplateUsed(responses, 'home.html')

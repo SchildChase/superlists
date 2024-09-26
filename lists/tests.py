741288ecc3202e37a570bce072de1fbc3612a962
schildchase <schildchase@qq.com>
2024-09-26 10:29:00 +0800

    redirect afer POST,and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+from http.client import responses
+
 from django.test import TestCase
 from lists.models import Item
 
@@ -9,15 +11,32 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(responses, 'home.html')
 
     def test_can_save_a_POST_request(self):
-        responses = self.client.post('/', data={'item_text':'A new list item'})
+        responses = self.client.post('/', data={'item_text': 'A new list item'})
 
-        self.assertEqual(Item.objects.count(),1)
+        self.assertEqual(Item.objects.count(), 1)
         new_item = Item.objects.first()
-        self.assertEqual(new_item.text,'A new list item')
+        self.assertEqual(new_item.text, 'A new list item')
+        # self.assertIn('A new list item', responses.content.decode())
+        # self.assertTemplateUsed(responses, 'home.html')
 
+    def test_redirects_after_POST(self):
+        responses = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(responses.status_code, 302)
+        self.assertEqual(responses['location'], '/')
 
-        self.assertIn('A new list item', responses.content.decode())
-        self.assertTemplateUsed(responses, 'home.html')
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_display_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        responses = self.client.get('/')
+
+        self.assertIn("itemey 1", responses.content.decode())
+        self.assertIn("itemey 2", responses.content.decode())
+
 
 class ItemModelTest(TestCase):
 
@@ -37,4 +56,3 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first(ever) list item')
         self.assertEqual(second_saved_item.text, 'item the second')
-
--- lists/views.py
+++ lists/views.py
@@ -1,17 +1,14 @@
-from django.shortcuts import render
+from django.core.signals import request_started
+from django.shortcuts import render, redirect
 from lists.models import Item
 
 from django.http import HttpResponse
 
 # Create your views here.
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text','')
-    item.save()
-    
-    #if request.method == 'POST':
-        #return HttpResponse(request.POST['item_text'])
-    return render(request, 'home.html',{
-        'new_item_text':request.POST.get('item_text',''),
-    })
-    #HttpResponse('<html><title>To-Do lists</title></html>')
+    if request.method == 'POST':
+        Item.objects.create(text = request.POST['item_text'])
+        return redirect('/')
+
+    items=Item.objects.all()
+    return render(request, 'home.html',{'items':items})
